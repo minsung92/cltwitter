9283256cfa627dc2a630b57eafe328e4833ff31e
Minsung <m.s.k.git.92@gmail.com>
2023-04-11 23:40:09 +0900

    firebase 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDzvCGF8AC0f3EmoLsrXKPNZHC9X8UH5w",
+  authDomain: "cltwitter-3fea7.firebaseapp.com",
+  projectId: "cltwitter-3fea7",
+  storageBucket: "cltwitter-3fea7.appspot.com",
+  messagingSenderId: "794099380837",
+  appId: "1:794099380837:web:60476b7c94f5efb25ecc8c"
+};
+
+export default firebase.initializeApp(firebaseConfig);
